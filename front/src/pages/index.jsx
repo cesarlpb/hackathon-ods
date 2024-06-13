4a465f2f3149650e5183dcb8f4378dd1e3a6ee0e
davidagrsan <davidagredanosanchez@gmail.com>
2024-06-13 11:30:05 +0200

    return y algo más del index

--- front/src/pages/index.jsx
+++ front/src/pages/index.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
 const Index = () => {
+    return (
     <div id="home-container">
         <header>
             <h1>Sala de estudio</h1>
         </header>
+        <main>
+            <section id="selection-container">
+                array.forEach(element => {
+                    
+                });
+            </section>
+            <aside id="creation-container">
+                <button id="newCategory"></button>
+                <button id="newFlashCard"></button>
+            </aside>
+        </main>
     </div>
+    )
 }
 
 export default Index
