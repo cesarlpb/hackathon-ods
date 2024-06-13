25687dd45a714fdcafc89c4eb6d5f7f5b9cabd15
CronoCode120 <ctaverafernandez@gmail.com>
2024-06-13 11:33:22 +0200

    added Room page

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Index from './pages/Index.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>page</div>
+    element: <Index />
   }
 ])
 
