9f9753fbac995b4bcde054c7a727bfc93df08a24
davidagrsan <davidagredanosanchez@gmail.com>
2024-06-13 11:24:31 +0200

    Creado index con la sala principal de juego

---
+++ front/src/pages/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Index = () => {
+    <div id="home-container">
+        <header>
+            <h1>Sala de estudio</h1>
+        </header>
+    </div>
+}
+
+export default Index
