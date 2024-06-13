271183c30a73ace395e99b4be8de0803ef4f2dd1
CronoCode120 <ctaverafernandez@gmail.com>
2024-06-13 11:58:52 +0200

    added fake flashcard data and implemented into Room

--- front/src/pages/Room.jsx
+++ front/src/pages/Room.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react"
+import data from '../cards.json'
+
 const Room = () => {
+    const [cardNum, setCardNum] = useState(0)
+    const [flashcard, setFlashcard] = useState(data.cards[cardNum])
+
+    useEffect(() => {
+        setFlashcard(data.cards[cardNum])
+    }, [cardNum])
+
+    const handleClick = () => {
+      setCardNum(cardNum + 1)
+    }
+
     return (
         <div>
-          <h1>ROOM XXXXXX</h1>
-          <h2>Prompt</h2>
+          <h1>ROOM 1</h1>
+          <h2>{flashcard.question}</h2>
           <textarea id="answer" cols={50} rows={15}></textarea>
+          <button type="button">Submit</button>
+          <button type="button" onClick={handleClick}>Next</button>
         </div>
     )
 }
